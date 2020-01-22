ea4c017368e4df51d81b8861d101a0e1891652cb
joanthanodonnell <47103253+S00180206@users.noreply.github.com>
2020-01-22 14:06:02 +0000

    week1lab1 work in progress

---
+++ OODLab1Week1/OODLab1Week1/Band.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODLab1Week1
+{
+    class Band:IComparable
+    {
+        public enum BandGenre { pop, rock , indie };
+        public string BandName { get; set; }
+
+        public int Formed { get; set; }
+
+        public string Members { get; set; }
+
+        public BandGenre bandGenre1;
+
+        public Band(string bandName, int formed, string members, BandGenre bandGenre)
+        {
+            BandName = bandName;
+            Formed = formed;
+            Members = members;
+            bandGenre1 = bandGenre;
+        }
+
+        public override string ToString()
+        {
+            return $"{BandName }" /*{()}*/;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Band objectThatIAmComparingTo = (Band)obj;
+
+            int returnValue = this.bandGenre1.CompareTo(objectThatIAmComparingTo.bandGenre1);
+
+            //return
+            return returnValue;
+        }
+    }
+}
