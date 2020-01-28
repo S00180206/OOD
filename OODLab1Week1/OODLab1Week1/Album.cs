a9c54f62ec31a105fa933aca84325b3bdcda3fef
jondonnell <S00180206@mail.itsligo.ie>
2020-01-28 09:36:39 +0000

    week1lab1Unfinisihed

---
+++ OODLab1Week1/OODLab1Week1/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODLab1Week1
+{
+    class Album
+    {
+        string AlbumName { get; set; }
+
+        int Released { get; set; }
+
+        double sales { get; set; }
+    }
+}
--- OODLab1Week1/OODLab1Week1/MainWindow.xaml.cs
+++ OODLab1Week1/OODLab1Week1/MainWindow.xaml.cs
@@ -20,7 +20,13 @@ namespace OODLab1Week1
     /// </summary>
     public partial class MainWindow : Window
     {
+        static Random r = new Random();
         List<Band> BandLists = new List<Band>();
+        List<Band> selectedBand = new List<Band>();
+        int selectedBands, selectedGenre/*, selectedAlbum*/;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,19 +34,45 @@ namespace OODLab1Week1
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Band band1 = new Band("Foo fighters",1994,"David Grohl, taylor hawkins, chris shiflett",Band.BandGenre.rock);
-            Band band2 = new Band("Linkin Park",1996,"chester bennington, mile shinoda, joe Hahn ", Band.BandGenre.rock);
-            Band band3 = new Band("Arctic Monkeys",2002,"Alex turner, matt helders, jamie cook", Band.BandGenre.indie);
-            Band band4 = new Band("Arcade Fire",2001, "win butler , regine Chassagne, william butler", Band.BandGenre.indie);
-            Band band5 = new Band("The Beatles",1957, "John Lennon, Paul McCartney, Ringo Starr, George Harrison", Band.BandGenre.pop);
-            Band band6 = new Band("ABBA",1972, "agnetha faltskog, ann-fir lyngstad, bjorn ulvaeus", Band.BandGenre.pop);
+            Band b1 = new Band("Foo fighters",1994,"David Grohl, taylor hawkins, chris shiflett",Band.BandGenre.rock);
+            Band b2 = new Band("Linkin Park",1996,"chester bennington, mile shinoda, joe Hahn ", Band.BandGenre.rock);
+            Band b3 = new Band("Arctic Monkeys",2002,"Alex turner, matt helders, jamie cook", Band.BandGenre.indie);
+            Band b4 = new Band("Arcade Fire",2001, "win butler , regine Chassagne, william butler", Band.BandGenre.indie);
+            Band b5 = new Band("The Beatles",1957, "John Lennon, Paul McCartney, Ringo Starr, George Harrison", Band.BandGenre.pop);
+            Band b6 = new Band("ABBA",1972, "agnetha faltskog, ann-fir lyngstad, bjorn ulvaeus", Band.BandGenre.pop);
 
-            BandLists.Add(band1);
-            BandLists.Add(band2);
-            BandLists.Add(band3);
-            BandLists.Add(band4);
-            BandLists.Add(band5);
-            BandLists.Add(band6);
+            BandLists.Add(b1);
+            BandLists.Add(b2);
+            BandLists.Add(b3);
+            BandLists.Add(b4);
+            BandLists.Add(b5);
+            BandLists.Add(b6);
+
+
+        }
+        private DateTime GetRandomDate(DateTime startDate, DateTime endDate)
+        {
+
+            TimeSpan t = endDate - startDate;
+            int releaseYear = t.Days;
+            DateTime newYear = startDate.AddDays(r.Next(releaseYear));
+            return newYear;
+        }
+
+        private void cbGenreband_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (var b in selectedBand)
+            {
+                BandLists.Add(b);
+            }
+
+            selectedBand.Clear();
+
+            selectedGenre = 0;
+            selectedBands = 0;
+            selectedGenre = 0;
+            //selectedAlbum = 0;
+           // RefreshDisplay();
         }
     }
 }
