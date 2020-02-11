55381f9828be337663458444ebc55a34d6fb4242
joanthanodonnell <47103253+S00180206@users.noreply.github.com>
2020-02-11 12:57:47 +0000

    OODLab2-Start

---
+++ OODLab2/Labsheet2/Ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumbersQuery();
+            NumbersLambda();
+
+        }
+
+        private static void NumbersLabda()
+        {
+            int[] numbers = { 1, 7, , 2, 9, 13, 16, 25, 2 };
+
+            var outputNumbers = numbers.Where(n => n > 5).OrderByDescending(n => n);
+
+            foreach(int number in outputNumbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OODLab2/Labsheet2/Ex2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = new DirectoryInfo("c:\\windows").GetFiles();
+
+            var query = from f in files
+                        where f.Length > 10000
+                        orderby f.Length, f.Name
+                        select new MyFileInfo
+                        {
+                            Name = f.Name,
+                            Length = f.Length,
+                            CreationTime = f.CreationTime
+                        };
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class MyFileInfo
+    {
+        public string Name { get; set; }
+        public long Length { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name of file is {Name}"; 
+        }
+    }
+}
