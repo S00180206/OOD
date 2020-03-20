ea1dfa0e0d018215b9a2022e32c9203b84957a57
jondonnell <S00180206@mail.itsligo.ie>
2020-03-20 16:07:04 +0000

    OODLabSheet5

---
+++ OODLabSheet5/OODLabSheet5/OODLabSheet5/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OODLabSheet5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BookshopContainer db = new BookshopContainer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from a in db.Authors
+                        select a;
+
+            dgAuthors.ItemsSource = query.ToList();
+        }
+    }
+}
